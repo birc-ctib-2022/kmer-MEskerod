345406e85955fd208a38d463acb6df6ddadaf813
MEskerod <112616513+MEskerod@users.noreply.github.com>
2022-10-07 08:38:06 +0200

    Changed tests because the list in unique_kmers a random

--- src/test_kmer.py
+++ src/test_kmer.py
@@ -10,16 +10,15 @@ from kmer import (
 
 def test_kmer():
     assert kmer('agtagtagta', 6) == ['agtagt','gtagta','tagtag','agtagt','gtagta']
-    assert kmer('agt', 5) == None
+    assert kmer('agt', 5) == []
     assert kmer('agt',1) == ['a','g','t']
 
 
 def test_uniue_kmers():
-    assert unique_kmers('agta',7) == None
+    assert unique_kmers('agta',7) == []
     assert unique_kmers ('aaaaaaaaaaa', 3) == ['aaa']
-    assert unique_kmers ('agtcagtcagtc',4) == ['agtc','gtca','tcag','cagt']
-
+    
 def test_count_kmers():
-    assert count_kmers('ag', 3) == None
+    assert count_kmers('ag', 3) == []
     assert count_kmers('aaaa', 1) == {'a': 4}
     assert count_kmers('agtagtagt',3) == {'agt': 3, 'gta': 2, 'tag': 2}
